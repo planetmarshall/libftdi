96dcc28ce43e6acfed1f286632b3686f891185c0
Andrew Marshall <planetmarshalluk@gmail.com>
2020-12-27 13:35:16 +0000

    export targets from find libusb

--- cmake/FindLIBUSB.cmake
+++ cmake/FindLIBUSB.cmake
@@ -10,28 +10,32 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
+# use pkg-config to get the directories and then use these values
+# in the FIND_PATH() and FIND_LIBRARY() calls
+find_package(PkgConfig)
+pkg_check_modules(PC_LIBUSB libusb-1.0)
 
-if (LIBUSB_INCLUDE_DIR AND LIBUSB_LIBRARIES)
+find_path(LIBUSB_INCLUDE_DIR libusb.h
+  PATH_SUFFIXES libusb-1.0
+  PATHS ${PC_LIBUSB_INCLUDEDIR} ${PC_LIBUSB_INCLUDE_DIRS})
 
-  # in cache already
-  set(LIBUSB_FOUND TRUE)
+find_library(LIBUSB_LIBRARY NAMES usb-1.0
+  PATHS ${PC_LIBUSB_LIBDIR} ${PC_LIBUSB_LIBRARY_DIRS})
 
-else (LIBUSB_INCLUDE_DIR AND LIBUSB_LIBRARIES)
-  # use pkg-config to get the directories and then use these values
-  # in the FIND_PATH() and FIND_LIBRARY() calls
-  find_package(PkgConfig)
-  pkg_check_modules(PC_LIBUSB libusb-1.0)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LIBUSB DEFAULT_MSG LIBUSB_LIBRARY LIBUSB_INCLUDE_DIR)
 
-  FIND_PATH(LIBUSB_INCLUDE_DIR libusb.h
-    PATH_SUFFIXES libusb-1.0
-    PATHS ${PC_LIBUSB_INCLUDEDIR} ${PC_LIBUSB_INCLUDE_DIRS})
+if (LIBUSB_FOUND)
+  set(LIBUSB_LIBRARIES ${LIBUSB_LIBRARY})
+  set(LIBUSB_INCLUDE_DIRS ${LIBUSB_INCLUDE_DIR})
+  set(LIBUSB_VERSION ${PC_LIBUSB_VERSION})
+  if (NOT TARGET LIBUSB::LIBUSB)
+    add_library(LIBUSB::LIBUSB UNKNOWN IMPORTED)
+    set_target_properties(LIBUSB::LIBUSB PROPERTIES
+            IMPORTED_LOCATION "${LIBUSB_LIBRARY}"
+            INTERFACE_INCLUDE_DIRECTORIES "${LIBUSB_INCLUDE_DIR}"
+            )
+  endif()
+endif()
 
-  FIND_LIBRARY(LIBUSB_LIBRARIES NAMES usb-1.0
-    PATHS ${PC_LIBUSB_LIBDIR} ${PC_LIBUSB_LIBRARY_DIRS})
-
-  include(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(LIBUSB DEFAULT_MSG LIBUSB_LIBRARIES LIBUSB_INCLUDE_DIR)
-
-  MARK_AS_ADVANCED(LIBUSB_INCLUDE_DIR LIBUSB_LIBRARIES)
-
-endif (LIBUSB_INCLUDE_DIR AND LIBUSB_LIBRARIES)
+mark_as_advanced(LIBUSB_INCLUDE_DIR LIBUSB_LIBRARY)
