0d919e72b1a0fdff6cda59660efb99860d3be923
Andrew Marshall <planetmarshalluk@gmail.com>
2020-12-28 16:39:24 +0000

    rename references to libusb

--- cmake/Findlibusb.cmake
+++ cmake/Findlibusb.cmake
@@ -1,22 +1,22 @@
 find_package(PkgConfig)
-pkg_check_modules(PC_LibUSB libusb-1.0)
+pkg_check_modules(PC_libusb libusb-1.0)
 
-find_path(LibUSB_INCLUDE_DIR libusb.h
+find_path(libusb_INCLUDE_DIR libusb.h
   PATH_SUFFIXES libusb-1.0
-  PATHS ${PC_LibUSB_INCLUDEDIR} ${PC_LibUSB_INCLUDE_DIRS})
+  PATHS ${PC_libusb_INCLUDEDIR} ${PC_libusb_INCLUDE_DIRS})
 
-find_library(LibUSB_LIBRARY NAMES usb-1.0
-  PATHS ${PC_LibUSB_LIBDIR} ${PC_LibUSB_LIBRARY_DIRS})
-mark_as_advanced(LibUSB_INCLUDE_DIR LibUSB_LIBRARY)
+find_library(libusb_LIBRARY NAMES usb-1.0
+  PATHS ${PC_libusb_LIBDIR} ${PC_libusb_LIBRARY_DIRS})
+mark_as_advanced(libusb_INCLUDE_DIR libusb_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LibUSB DEFAULT_MSG LibUSB_LIBRARY LibUSB_INCLUDE_DIR)
+find_package_handle_standard_args(libusb DEFAULT_MSG libusb_LIBRARY libusb_INCLUDE_DIR)
 
-if (LibUSB_FOUND AND NOT TARGET LibUSB::LibUSB)
-    add_library(LibUSB::LibUSB UNKNOWN IMPORTED)
-    set_target_properties(LibUSB::LibUSB PROPERTIES
-            IMPORTED_LOCATION "${LibUSB_LIBRARY}"
-            INTERFACE_INCLUDE_DIRECTORIES "${LibUSB_INCLUDE_DIR}"
+if (libusb_FOUND AND NOT TARGET libusb::libusb)
+    add_library(libusb::libusb UNKNOWN IMPORTED)
+    set_target_properties(libusb::libusb PROPERTIES
+            IMPORTED_LOCATION "${libusb_LIBRARY}"
+            INTERFACE_INCLUDE_DIRECTORIES "${libusb_INCLUDE_DIR}"
             )
 endif()
 
