355baca8e5153867999177e62655a35096c1de7e
Andrew Marshall <planetmarshalluk@gmail.com>
2021-02-01 22:04:28 +0000

    Add usleep impl and headers from minix

---
+++ src/posix/usleep_win.c
@@ -0,0 +1,15 @@
+#include <Windows.h>
+
+// From https://gist.github.com/ngryman/6482577
+void usleep(unsigned int usec)
+{
+	HANDLE timer;
+	LARGE_INTEGER ft;
+
+	ft.QuadPart = -(10 * (__int64)usec);
+
+	timer = CreateWaitableTimer(NULL, TRUE, NULL);
+	SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
+	WaitForSingleObject(timer, INFINITE);
+	CloseHandle(timer);
+}
